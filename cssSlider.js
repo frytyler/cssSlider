716b1fdbf0896be5d03f9367d66a01e4f9799000
Tyler Fry <fry.tyler@gmail.com>
2012-09-14 22:57:58 -0400

    Set init variables

--- cssSlider.js
+++ cssSlider.js
@@ -1,10 +1,17 @@
 (function($){
 	var method = {
 		init: function(){
-			
+			method.selectedIndex = 0;
+			//a.animationSupport = Modernizr.cssanimations && Modernizr.opacity;	
+			method.nextSlide = $('#nextSlide');
+			method.prevSlide = $('#prevSlide');
+			method.nextSlide.click(_.bind(method.nextPanel, this));
+			method.prevSlide.click(_.bind(method.prevPanel,this));	
+			method.autoplay();
+			method.tooltip();
 		}
 	};
 	$.fn.cssSlider = function(method){
-
+		method.init();
 	};
 })(jQuery)
